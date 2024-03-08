b3640384ac75a82f9b6283a7b203b379f08f9b4d
Phanindra Duvvuri <phanindra40430@gmail.com>
2024-03-08 15:24:34 +0530

    Made Login Page

--- csa-website/src/components/LoginPage.jsx
+++ csa-website/src/components/LoginPage.jsx
@@ -1,5 +1,25 @@
+import "./LoginPage.css";
+import CSALogo from "../assets/CSA.png";
+import GoogleLogo from "../assets/google-logo.png";
+import FBLogo from "../assets/fb-logo.png";
+
 const LoginPage = () => {
-  return <h1>Login Page</h1>;
+  return (
+    <div className="login-card">
+      <img className="logo" src={CSALogo} alt="Career Sure Acedamy Logo" />
+      <button className="login-btn">
+        <img src={GoogleLogo} alt="google logo" />
+        <div>Continue with Google</div>
+      </button>
+      <p>
+        <span>OR</span>
+      </p>
+      <button className="login-btn">
+        <img src={FBLogo} alt="facebook logo" />
+        <div>Continue with Facebook</div>
+      </button>
+    </div>
+  );
 };
 
 export default LoginPage;
