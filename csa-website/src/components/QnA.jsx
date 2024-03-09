21ac78364704172b5683bd0f59ce1eed65aea972
Phanindra Duvvuri <phanindra40430@gmail.com>
2024-03-09 20:30:07 +0530

    Added QnA Section.

--- csa-website/src/components/QnA.jsx
+++ csa-website/src/components/QnA.jsx
@@ -1,5 +1,24 @@
+import "./QnA.css";
+
 const QnA = () => {
-  return <h1>QnA</h1>;
+  return (
+    <div className="qna-container">
+      <div className="join-meeting">
+        <p>Resolve your queries instantly...</p>
+        <button>Join</button>
+      </div>
+      <div className="post-ques">
+        <textarea
+          name="ques"
+          id="ques"
+          cols="30"
+          rows="10"
+          placeholder="Post your question here..."
+        ></textarea>
+        <button>Post</button>
+      </div>
+    </div>
+  );
 };
 
 export default QnA;
